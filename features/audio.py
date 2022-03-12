49b28497b3fd38685bf88fdbe462f15e83694411
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2022-03-12 22:29:32 +0600

    feat: add and test features boilerplate

--- features/audio.py
+++ features/audio.py
@@ -5,15 +5,24 @@ import librosa
 class MFCC(object):
     """MFCC.
     Args:
-        .
+        sr: [int]
+            sampling rate
+
+        hop_length: [int]
+            number of samples between successive frames
+
+        n_mfcc: [int] 
+            number of MFCCs to return
     """
 
-    def __init__(self, params):
-        self.params = params
+    def __init__(self, sr=16000, hop_length=256, n_mfcc=20):
+        self.sr = sr
+        self.hop_length = hop_length
+        self.n_mfcc = n_mfcc
 
     def __call__(self, sample):
         y, label = sample
-        sr, hop_length, n_mfcc = self.params
+        sr, hop_length, n_mfcc = self.sr, self.hop_length, self.n_mfcc
         # Compute MFCC features from the raw signal
         mfcc = librosa.feature.mfcc(
             y=y, sr=sr, hop_length=hop_length, n_mfcc=n_mfcc
