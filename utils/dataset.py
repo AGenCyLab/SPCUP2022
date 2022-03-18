6fd1bd9d37e403c5e9dd00fa6590b5c4a01c00b1
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2022-03-18 09:41:15 +0600

    fix: fixes #8

--- utils/dataset.py
+++ utils/dataset.py
@@ -11,21 +11,27 @@ class SPCUP22DatasetDownloader:
     config yaml file with the following structure:
 
     spcup22:
-        training:
-            part1:
-                link: https://www.dropbox.com/s/36yqmymkva2bwdi/spcup_2022_training_part1.zip?dl=1
-                filename: spcup_2022_training_part1.zip
-                default_path: data/spcup22/training/part1/spcup_2022_training_part1 
-            part2:
-                link: https://www.dropbox.com/s/wsmlthhri29fb79/spcup_2022_unseen.zip?dl=1
-                filename: spcup_2022_unseen.zip
-                default_path: data/spcup22/training/part2/spcup_2022_unseen
-        evaluation:
-            part1:
-                link: https://www.dropbox.com/s/ftkyvwxgr9wl7jf/spcup_2022_eval_part1.zip?dl=1
-                filename: spcup_2022_eval_part1.zip
-                default_path: data/spcup22/training/part1/spcup_2022_eval_part1
+        raw_audio:
+            training:
+                part1:
+                    link: https://www.dropbox.com/s/36yqmymkva2bwdi/spcup_2022_training_part1.zip?dl=1
+                    filename: spcup_2022_training_part1.zip
+                    default_path: data/spcup22/training/part1/spcup_2022_training_part1 
+                part2:
+                    link: https://www.dropbox.com/s/wsmlthhri29fb79/spcup_2022_unseen.zip?dl=1
+                    filename: spcup_2022_unseen.zip
+                    default_path: data/spcup22/training/part2/spcup_2022_unseen
+            evaluation:
+                part1:
+                    link: https://www.dropbox.com/s/ftkyvwxgr9wl7jf/spcup_2022_eval_part1.zip?dl=1
+                    filename: spcup_2022_eval_part1.zip
+                    default_path: data/spcup22/evaluation/spcup_2022_eval_part1
 
+        mel_features:
+            training:
+                ...
+            evaluation:
+                ...
     """
 
     def __init__(
@@ -33,7 +39,21 @@ class SPCUP22DatasetDownloader:
         config_file_path: str,
         dataset_name: str = "spcup22",
         unzip_after_download: bool = True,
+        data_type: str = "raw_audio",
     ) -> None:
+        """
+        config_file_path: str
+            the path to the dataset.yaml file
+
+        dataset_name: str
+            by default it's "spcup22", however, if other datasets are introduced
+            we can change the name according to the entry in the dataset.yaml
+            file
+
+        data_type: str 
+            one of ("raw_audio", "mel_features", ...) (add other types as needed)
+            the dataset.yaml file should be updated accordingly
+        """
         self.root = pathlib.Path(__file__).parent.parent
         self.config_file_path = config_file_path
         self.dataset_name = dataset_name
@@ -41,10 +61,10 @@ class SPCUP22DatasetDownloader:
         with open(self.config_file_path, mode="r") as yaml_file_object:
             self.config = yaml.load(yaml_file_object, Loader=yaml.FullLoader)[
                 dataset_name
-            ]
+            ][data_type]
 
         self.download_folder_root = pathlib.Path(self.root).joinpath(
-            "data", self.dataset_name
+            "data", data_type, self.dataset_name
         )
 
         if not self.download_folder_root.exists():
@@ -90,4 +110,5 @@ class SPCUP22DatasetDownloader:
         print("Unzipping [{}] ...".format(zip_file_path))
         with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
             zip_ref.extractall(extraction_dir)
-            os.remove(zip_file_path)
+
+        os.remove(zip_file_path)
