cd062a75d4a432741c2629583353f62e3a4fa141
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2022-03-28 14:39:53 +0600

    fix: fix a typo with dataset downloader

--- utils/dataset.py
+++ utils/dataset.py
@@ -122,7 +122,7 @@ class SPCUP22DatasetDownloader:
         with open(self.config_file_path, mode="r") as yaml_file_object:
             self.config = yaml.load(yaml_file_object, Loader=yaml.FullLoader)[
                 dataset_name
-            ]
+            ][data_type]
 
         self.download_folder_root = pathlib.Path(self.root).joinpath(
             "data", data_type, self.dataset_name
